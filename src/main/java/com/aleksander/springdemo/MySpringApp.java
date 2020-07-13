87d80cda3bac262d0788ba05d9b2bbf8c7be91b3
Aleksander Piekarz <aleksander.m.piekarz@gmail.com>
2020-07-13 23:03:12 +0200

    Added option to change implementation of Coach interface via spring configuration file ( spring bean )

---
+++ src/main/java/com/aleksander/springdemo/MySpringApp.java
@@ -0,0 +1,22 @@
+package com.aleksander.springdemo;
+
+import com.aleksander.springdemo.coaches.interfaces.Coach;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MySpringApp {
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close the context
+        context.close();
+    }
+}
